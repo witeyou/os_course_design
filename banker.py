704422139fa2cf7d8109f6c01bf89612c2b8761a
sxc <1171838741@qq.com>
2019-01-04 14:19:36 +0800

    能够判断一个进程能否在破坏系统安全性的情况下进入系统

--- banker.py
+++ banker.py
@@ -4,7 +4,8 @@
 描述:课设题目银行家算法的核心运算部分
 作者:计嵌162 史学超 20160323
 日期:2018-12-28
-TODO:对应新增加的进程没有处理
+修改:改正了read_file()方法使之正常运行
+     修改了main()方法中,能够使之接收待进入系统的进程,并且能够判断能否允许进程进入系统
 """
 
 
@@ -23,11 +24,11 @@ def write_file(total_list_, claim_list2_, own_list2_):
 def read_file():
     # 从对应的文件中读出对应的数据并包装成合适的格式
     from simplejson import load
-    with open('./resource_total_list.txt', 'w') as ft:
+    with open('./resource_total_list.txt', 'r') as ft:
         total_list = load(ft)
-    with open('./resource_claim_list.txt', 'w') as fc:
+    with open('./resource_claim_list.txt', 'r') as fc:
         claim_list2 = load(fc)
-    with open('./resource_own_list.txt', 'w') as fo:
+    with open('./resource_own_list.txt', 'r') as fo:
         own_list2 = load(fo)
     return total_list, claim_list2, own_list2
 
@@ -75,25 +76,30 @@ def check_safety(own_list2_, need_list2_, free_list_):
                 return False
 
 
-def main(total_list_, claim_list2_, own_list2_, add_list_=None):
+def main(total_list_=None, claim_list2_=None, own_list2_=None, add_list_=None):
     """
-    程序运行的主函数部分
+    程序运行的主函数部分,能够从对应的文件中读取所需的参数,也能接受传递的参数.能够判断当前程序是否安全,
+    判断能否添加新的进程进入系统
     :param total_list_:  List[int]         系统总拥有资源向量
     :param claim_list2_: List[List[int]]   系统内所有进程所需最大资源矩阵
-    :param own_list2_:    List[List[int]]   系统内所有进程已拥有资源矩阵
+    :param own_list2_:   List[List[int]]   系统内所有进程已拥有资源矩阵
     :param add_list_:    List[int]         等待进入系统的资源向量
-    :return:
+    :return:             Boolean           true对应系统安全,反之
+    例子:前三个参数已经设置了从文件读取,最后一个添加资源向量如果有还是选择通过参数传递
+    e.g.main(add_list_ =  _resource_add_list)
     """
-
-    resource_total_list = total_list_
-    resource_claim_list2 = claim_list2_
-    resource_own_list2 = own_list2_
+    if total_list_ is None and claim_list2_ is None and own_list2_ is None:
+        resource_total_list, resource_claim_list2, resource_own_list2 = read_file()
+    else:
+        resource_total_list = total_list_
+        resource_claim_list2 = claim_list2_
+        resource_own_list2 = own_list2_
     print("系统拥有的总共资源向量为:", resource_total_list)
     print("各个进程需要最大资源矩阵为:", resource_claim_list2)
     print("各个进程已经拥有资源矩阵为:", resource_own_list2)
     number_resource = len(resource_total_list)
     number_process = len(resource_claim_list2)
-    print("系统拥有的资源种类:{}\n系统待命的进程数量:{}".format(number_resource, number_process))
+    print("系统中拥有的资源种类:{}\n系统正待命的进程数量:{}".format(number_resource, number_process))
     resource_need_list2 = [[resource_claim_list2[r][c] - resource_own_list2[r][c] for c in range(number_resource)]
                            for r in range(number_process)]
     print("各个进程仍需要资源矩阵为:", resource_need_list2)
@@ -118,8 +124,18 @@ def main(total_list_, claim_list2_, own_list2_, add_list_=None):
             return False
     else:
         pass
-        # TODO:对应书上的步骤三部分,判断一个进程能否进入当前系统
-    return
+        for item_add, item_free in zip(add_list_, resource_free_list):
+            if item_add > item_free:
+                print("当前系统无法添加新的进程")
+                return False
+        resource_own_list2.append([0 for _ in range(number_resource)])
+        resource_need_list2.append(add_list_)
+        if check_safety(resource_own_list2, resource_need_list2, resource_free_list):
+            print("该进程的添加是允许的")
+            return True
+        else:
+            print("该程序的插入会破坏系统安全性")
+            return False
 
 
 if __name__ == '__main__':
@@ -138,6 +154,8 @@ if __name__ == '__main__':
         [3, 3, 4, 2],
         [5, 1, 2, 1]
     ]
-    # write_file(_resource_total_list, _resource_claim_list2, _resource_own_list2)
-    main(_resource_total_list, _resource_claim_list2, _resource_own_list2)
+    _resource_add_list = [1, 1, 1, 1]
+    write_file(_resource_total_list, _resource_claim_list2, _resource_own_list2)
+    # main(_resource_total_list, _resource_claim_list2, _resource_own_list2,_resource_add_list)
+    main()
     print("---银行家算法运行结束---")
